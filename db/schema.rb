fbc21788633d8ec6d7154518e6b7bf59f1ad4981
Louis Rivest <louisrivest4@gmail.com>
2023-06-25 20:47:33 -0400

    l

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_001605) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_004246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_001605) do
     t.string "name"
     t.integer "inventory"
     t.integer "price"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
